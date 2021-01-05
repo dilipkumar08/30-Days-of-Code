ac54ea923d05e8fca4a8175a9bea3b40712e3923
dilipkumar08 <77008477+dilipkumar08@users.noreply.github.com>
2021-01-05 20:12:32 +0530

    Hello world

---
+++ HelloWorld.kt
@@ -0,0 +1,7 @@
+import java.io.*
+import java.util.*
+fun main(args: Array<String>) {
+        /* Enter your code here. Read input from STDIN. Print output to STDOUT. */
+val userInput=readLine()
+println("Hello, World.\n$userInput")
+}
