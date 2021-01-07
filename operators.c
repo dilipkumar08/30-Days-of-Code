c0a2bab8aed53aa1d3fed995c84a09142d49cae1
dilipkumar08 <77008477+dilipkumar08@users.noreply.github.com>
2021-01-07 19:25:56 +0530

    Create operators.c

---
+++ operators.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+    double mealCost,tax,tips;
+    int fmc;
+    clrscr();
+    scanf("%lf%lf%lf",&mealCost,&tax,&tips);
+    tax=(tax*mealCost)/100;
+    tips=(tips*mealCost)/100;
+    fmc=mealCost+tips+tax;
+    printf("%d",fmc);
+  getch();
+  return 0;    
+    
+}
